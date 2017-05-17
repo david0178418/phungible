ec1ee8c20f33c43823990d64598c931672a81932
David Granado <davidjgranado@gmail.com>
2017-05-16 22:01:47 -0500

    Update color scheme

--- src/client/shared/theme.ts
+++ src/client/shared/theme.ts
@@ -4,11 +4,11 @@ export default
 getMuiTheme({
 	palette: {
 		accent1Color: '#FF9800',
-		accent2Color: '#ff0000',
+		accent2Color: '#FF9800',
 		accent3Color: '#00ff00',
 		alternateTextColor: '#FFFFFF',
-		primary1Color: '#00BCD4',
-		primary2Color: '#0097A7',
+		primary1Color: '#0097A7',
+		primary2Color: '#00BCD4',
 		primary3Color: '#B2EBF2',
 	},
 });
