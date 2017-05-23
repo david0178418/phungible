3b76f8ec0cf934d021f7da3569a86e32af144703
David Granado <davidjgranado@gmail.com>
2017-05-23 12:38:27 -0500

    Adjust timings for update prompting and page loading

--- src/client/index.ts
+++ src/client/index.ts
@@ -13,7 +13,7 @@ OfflinePluginRuntime.install({
 	onUpdated() {
 		setTimeout(() => {
 			renderApp(true);
-		}, 3000);
+		}, 2000);
 	},
 });
 
@@ -41,7 +41,7 @@ function beginTransition() {
 	const pageLoadTime = (window as any).pageLoadTime;
 	const elapsedTime = Date.now() - pageLoadTime;
 
-	setTimeout(runTrans, 1000 - elapsedTime);
+	setTimeout(runTrans, 500 - elapsedTime);
 
 	function runTrans() {
 		loadingContainer.className = 'app-loading-container hide';
