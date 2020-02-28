bf36f913a85707b150d9d0d8c653551d4afecc55
David Granado <davidjgranado@gmail.com>
2020-02-28 16:59:44 -0600

    Add transactions tab content

--- src/pages/daily-activity.page.tsx
+++ src/pages/daily-activity.page.tsx
@@ -20,6 +20,7 @@ import {
 	IonSelect,
 	IonSelectOption,
 	IonTextarea,
+	IonDatetime,
 } from '@ionic/react';
 
 enum PageTab {
@@ -31,6 +32,8 @@ export
 function HomePage() {
 	const [selectedTab, setSelectedTab] = useState<PageTab>(PageTab.Budgets);
 	const [showExpense, setShowExpense] = useState(false);
+	const [showQuickExpense, setShowQuickExpense] = useState(false);
+	const [selectedDate, setSelectedDate] = useState(() => (new Date()).toISOString());
 
 	return (
 		<IonPage>
@@ -57,68 +60,139 @@ function HomePage() {
 					</IonSegmentButton>
 					<IonSegmentButton value={PageTab.Transactions}>
 						<IonLabel>
-							Transactions
+							History
 						</IonLabel>
 					</IonSegmentButton>
 				</IonSegment>
-				{showExpense && (
-					<>
-						<IonGrid>
-							<IonRow>
-								<IonCol>
-									<IonItem>
-										<IonLabel position="stacked">
-											Label
-										</IonLabel>
-										<IonInput/>
-									</IonItem>
-								</IonCol>
-								<IonCol size="3">
-									<IonItem>
-										<IonLabel position="stacked">
-											$
-										</IonLabel>
-										<IonInput type="number"/>
-									</IonItem>
-								</IonCol>
-							</IonRow>
-						</IonGrid>
-						<IonItem>
-							<IonLabel position="stacked">From Account</IonLabel>
-							<IonSelect value="">
-								<IonSelectOption value="">None</IonSelectOption>
-								<IonSelectOption value="m">My Acount</IonSelectOption>
-							</IonSelect>
-						</IonItem>
-						<IonItem>
-							<IonLabel position="stacked">Notes</IonLabel>
-							<IonTextarea />
-						</IonItem>
-					</>
+				{(selectedTab === PageTab.Budgets) && (
+					<div>
+						{showExpense && (
+							<>
+								<IonGrid>
+									<IonRow>
+										<IonCol>
+											<IonItem>
+												<IonLabel position="stacked">
+													Label
+												</IonLabel>
+												<IonInput/>
+											</IonItem>
+										</IonCol>
+										<IonCol size="3">
+											<IonItem>
+												<IonLabel position="stacked">
+													$
+												</IonLabel>
+												<IonInput type="number"/>
+											</IonItem>
+										</IonCol>
+									</IonRow>
+								</IonGrid>
+								<IonItem>
+									<IonLabel position="stacked">From Account</IonLabel>
+									<IonSelect value="">
+										<IonSelectOption value="">None</IonSelectOption>
+										<IonSelectOption value="m">My Acount</IonSelectOption>
+									</IonSelect>
+								</IonItem>
+								<IonItem>
+									<IonLabel position="stacked">Notes</IonLabel>
+									<IonTextarea />
+								</IonItem>
+							</>
+						)}
+						<p>
+							<IonButton expand="full" onClick={() => setShowExpense(!showExpense)}>
+								Add Unplanned Expense
+							</IonButton>
+						</p>
+						<IonList>
+							<IonItem lines="none">
+								<IonLabel>
+									<p>Remaining Budgeted Amounts</p>
+								</IonLabel>
+							</IonItem>
+							<IonItem onClick={() => console.log(111)}>
+								<IonLabel>
+									Foo
+									<p>
+										Renews Feb 20, 2021
+									</p>
+								</IonLabel>
+								<div className="money" slot="end">
+									$100
+								</div>
+							</IonItem>
+						</IonList>
+					</div>
 				)}
-				<p>
-					<IonButton expand="full" onClick={() => setShowExpense(!showExpense)}>
-						Add Unplanned Expense
-					</IonButton>
-				</p>
-				<IonList>
-					<IonItem lines="none">
-						<IonLabel>
-							<p>Remaining Budgeted Amounts</p>
-						</IonLabel>
-					</IonItem>
-					<IonItem onClick={() => console.log(111)}>
-						<IonLabel>
-							Foo
-							<p>
-								Renews Feb 20, 2021
-							</p>
-						</IonLabel>
-						<div className="money" slot="end">
-							$100
-						</div>
-					</IonItem>
-				</IonList>
+
+				{(selectedTab === PageTab.Transactions) && (
+					<div>
+						{showQuickExpense && (
+							<>
+								<IonGrid>
+									<IonRow>
+										<IonCol>
+											<IonItem>
+												<IonLabel position="stacked">
+													Label
+												</IonLabel>
+												<IonInput/>
+											</IonItem>
+										</IonCol>
+										<IonCol size="3">
+											<IonItem>
+												<IonLabel position="stacked">
+													$
+												</IonLabel>
+												<IonInput type="number"/>
+											</IonItem>
+										</IonCol>
+									</IonRow>
+								</IonGrid>
+								<IonItem>
+									<IonLabel position="stacked">From Account</IonLabel>
+									<IonSelect value="">
+										<IonSelectOption value="">None</IonSelectOption>
+										<IonSelectOption value="m">My Acount</IonSelectOption>
+									</IonSelect>
+								</IonItem>
+								<IonItem>
+									<IonLabel position="stacked">Notes</IonLabel>
+									<IonTextarea />
+								</IonItem>
+							</>
+						)}
+						<p>
+							<IonButton expand="full" onClick={() => setShowQuickExpense(!showQuickExpense)}>
+								Add Quick Expense
+							</IonButton>
+						</p>
+						<IonList>
+							<IonItem lines="none">
+								<IonLabel>
+									<p>TransactionsFor</p>
+								</IonLabel>
+								<IonDatetime
+									value={selectedDate}
+									onIonChange={({detail}) => detail.value && setSelectedDate(detail.value)}
+								/>
+							</IonItem>
+							<IonItem onClick={() => console.log(111)}>
+								<IonLabel>
+									Foo
+									<p>
+										Renews Feb 20, 2021
+									</p>
+								</IonLabel>
+								<div className="money" slot="end">
+									$100
+								</div>
+							</IonItem>
+						</IonList>
+					</div>
+				)}
 			</IonContent>
 		</IonPage>
 	);
