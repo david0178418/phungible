584706a42c65e2e722f5dff343e6dd12156073ab
David Granado <dgranado@glgroup.com>
2020-03-18 18:20:52 -0500

    Factor out routes

--- src/app.tsx
+++ src/app.tsx
@@ -1,12 +1,9 @@
 
 import React, {
-	lazy,
 	Suspense,
 } from 'react';
-import { Route } from 'react-router-dom';
 import {
 	IonApp,
-	IonRouterOutlet,
 	IonSplitPane,
 	IonLoading,
 } from '@ionic/react';
@@ -17,46 +14,7 @@ import './config';
 import './global.scss';
 
 import { ContextProvider } from './context-provider';
-
-interface ComponentImport {
-    default: () => JSX.Element;
-}
-type ComponentFactory = () => Promise<ComponentImport>;
-type LazyComponent = React.LazyExoticComponent<() => JSX.Element>;
-
-function prefetchFn(timing: number) {
-	const prefetchList: ComponentFactory[] = [];
-
-	setTimeout(async () => {
-		for(let component of prefetchList) {
-			await component();
-		}
-	}, timing);
-
-	return (componentImport: ComponentFactory): LazyComponent => {
-		prefetchList.push(componentImport);
-		return lazy(componentImport);
-	};
-}
-
-const prefetch = prefetchFn(5000);
-
-const AccountsPage = prefetch(() => import('./pages/accounts.page'));
-const AccountEditPage = prefetch(() => import('./pages/account-edit.page'));
-const BudgetsPage = prefetch(() => import('./pages/budgets.page'));
-const BudgetEditPage = prefetch(() => import('./pages/budget-edit.page'));
-const DailyActivityPage = prefetch(() => import('./pages/daily-activity.page'));
-const RecurringTransactions = prefetch(() => import('./pages/recurring-transactions.page'));
-const RecurringTransactionEdit = prefetch(() => import('./pages/recurring-transaction-edit.page'));
-const SettingsPage = prefetch(() => import('./pages/settings.page'));
-const ProfilesPage = prefetch(() => import('./pages/profiles.page'));
-const ProfileEditPage = prefetch(() => import('./pages/profile-edit.page'));
-const TransactionsPage = prefetch(() => import('./pages/transactions.page'));
-const TransactionEditPage = prefetch(() => import('./pages/transaction-edit.page'));
-const TrendsPage = prefetch(() => import('./pages/trends.page'));
-const WelcomePage = prefetch(() => import('./pages/welcome.page'));
-const HelpPage = prefetch(() => import('./pages/help.page'));
-const LoginPage = prefetch(() => import('./pages/login.page'));
+import { Routes } from './routes';
 
 export
 function App() {
@@ -67,73 +25,7 @@ function App() {
 					<IonSplitPane contentId="main">
 						<Menu />
 						<Suspense fallback={<IonLoading isOpen />}>
-							<IonRouterOutlet id="main">
-								<Route
-									path="/accounts"
-									component={AccountsPage}
-								/>
-								<Route
-									path="/account/:id?"
-									component={AccountEditPage}
-								/>
-								<Route
-									path="/budgets"
-									component={BudgetsPage}
-								/>
-								<Route
-									path="/budget/:id?"
-									component={BudgetEditPage}
-								/>
-								<Route
-									path="/help"
-									component={HelpPage}
-								/>
-								<Route
-									path="/login"
-									component={LoginPage}
-								/>
-								<Route
-									path="/recurring-transactions"
-									component={RecurringTransactions}
-								/>
-								<Route
-									path="/recurring-transaction/:id?"
-									component={RecurringTransactionEdit}
-								/>
-								<Route
-									path="/settings"
-									component={SettingsPage}
-								/>
-								<Route
-									path="/profiles"
-									component={ProfilesPage}
-								/>
-								<Route
-									path="/profile/:id?"
-									component={ProfileEditPage}
-								/>
-								<Route
-									path="/transactions"
-									component={TransactionsPage}
-								/>
-								<Route
-									path="/transaction/:id?"
-									component={TransactionEditPage}
-								/>
-								<Route
-									path="/trends"
-									component={TrendsPage}
-								/>
-								<Route
-									path="/welcome"
-									component={WelcomePage}
-								/>
-								<Route
-									exact
-									path="/"
-									component={DailyActivityPage}
-								/>
-							</IonRouterOutlet>
+							<Routes />
 						</Suspense>
 					</IonSplitPane>
 				</IonReactRouter>
---
+++ src/routes.tsx
@@ -0,0 +1,117 @@
+import React, { lazy } from 'react';
+import { IonRouterOutlet } from '@ionic/react';
+import { Route } from 'react-router-dom';
+
+interface ComponentImport {
+    default: () => JSX.Element;
+}
+type ComponentFactory = () => Promise<ComponentImport>;
+type LazyComponent = React.LazyExoticComponent<() => JSX.Element>;
+
+function prefetchFn(timing: number) {
+	const prefetchList: ComponentFactory[] = [];
+
+	setTimeout(async () => {
+		for(let component of prefetchList) {
+			await component();
+		}
+	}, timing);
+
+	return (componentImport: ComponentFactory): LazyComponent => {
+		prefetchList.push(componentImport);
+		return lazy(componentImport);
+	};
+}
+
+const prefetch = prefetchFn(5000);
+
+const AccountsPage = prefetch(() => import('./pages/accounts.page'));
+const AccountEditPage = prefetch(() => import('./pages/account-edit.page'));
+const BudgetsPage = prefetch(() => import('./pages/budgets.page'));
+const BudgetEditPage = prefetch(() => import('./pages/budget-edit.page'));
+const DailyActivityPage = prefetch(() => import('./pages/daily-activity.page'));
+const RecurringTransactions = prefetch(() => import('./pages/recurring-transactions.page'));
+const RecurringTransactionEdit = prefetch(() => import('./pages/recurring-transaction-edit.page'));
+const SettingsPage = prefetch(() => import('./pages/settings.page'));
+const ProfilesPage = prefetch(() => import('./pages/profiles.page'));
+const ProfileEditPage = prefetch(() => import('./pages/profile-edit.page'));
+const TransactionsPage = prefetch(() => import('./pages/transactions.page'));
+const TransactionEditPage = prefetch(() => import('./pages/transaction-edit.page'));
+const TrendsPage = prefetch(() => import('./pages/trends.page'));
+const WelcomePage = prefetch(() => import('./pages/welcome.page'));
+const HelpPage = prefetch(() => import('./pages/help.page'));
+const LoginPage = prefetch(() => import('./pages/login.page'));
+
+
+export
+function Routes() {
+	return (
+		<IonRouterOutlet id="main">
+			<Route
+				path="/accounts"
+				component={AccountsPage}
+			/>
+			<Route
+				path="/account/:id?"
+				component={AccountEditPage}
+			/>
+			<Route
+				path="/budgets"
+				component={BudgetsPage}
+			/>
+			<Route
+				path="/budget/:id?"
+				component={BudgetEditPage}
+			/>
+			<Route
+				path="/help"
+				component={HelpPage}
+			/>
+			<Route
+				path="/login"
+				component={LoginPage}
+			/>
+			<Route
+				path="/recurring-transactions"
+				component={RecurringTransactions}
+			/>
+			<Route
+				path="/recurring-transaction/:id?"
+				component={RecurringTransactionEdit}
+			/>
+			<Route
+				path="/settings"
+				component={SettingsPage}
+			/>
+			<Route
+				path="/profiles"
+				component={ProfilesPage}
+			/>
+			<Route
+				path="/profile/:id?"
+				component={ProfileEditPage}
+			/>
+			<Route
+				path="/transactions"
+				component={TransactionsPage}
+			/>
+			<Route
+				path="/transaction/:id?"
+				component={TransactionEditPage}
+			/>
+			<Route
+				path="/trends"
+				component={TrendsPage}
+			/>
+			<Route
+				path="/welcome"
+				component={WelcomePage}
+			/>
+			<Route
+				exact
+				path="/"
+				component={DailyActivityPage}
+			/>
+		</IonRouterOutlet>
+	);
+}
